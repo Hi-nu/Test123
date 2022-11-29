1b8f82a59b9a70f256e9149fcf0476a79164f961
Hi-nu <roughskethc1104@gmail.com>
2022-11-29 15:16:36 +0900

    test

---
+++ src/testt/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+
+    return (
+        <div>ff</div>
+    )
+
+}
+
+
+export default Footer;
---
+++ src/testt/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return (
+        <div>hh</div>
+    )
+
+}
+
+
+export default Header;
